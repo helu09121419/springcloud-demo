8b30b10b5b622995311aa920195eb3ec163dad5a
helu09121419 <694387263@qq.com>
2020-05-21 15:34:13 +0800

    remove files

--- src/main/java/com/ithl/eurekaservicefeignapi/service/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.ithl.eurekaservicefeignapi.service;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-public interface HelloService {
-    @GetMapping("/hello")
-    String hello(@RequestParam(value = "name") String name);
-}
