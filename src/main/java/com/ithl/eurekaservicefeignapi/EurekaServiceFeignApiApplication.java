13ac758a3bb9b4ce970b79f7c37a735045561a50
helu09121419 <694387263@qq.com>
2020-05-21 11:49:32 +0800

    提交springcloudfeign 继承性公用api

--- src/main/java/com/ithl/eurekaservicefeignapi/EurekaServiceFeignApiApplication.java
+++ src/main/java/com/ithl/eurekaservicefeignapi/EurekaServiceFeignApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EurekaServiceFeignApiApplication {
 
     public static void main(String[] args) {
+        System.out.println("-----------");
         SpringApplication.run(EurekaServiceFeignApiApplication.class, args);
     }
 
